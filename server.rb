c98782d3ec69923c743ab294cff2c931ec728093
@nolim1t <barry.teoh+github@gmail.com>
2012-10-15 15:05:20 +1100

    There is an easier way haha

--- server.rb
+++ server.rb
@@ -18,35 +18,19 @@ class HTTPHandler < EM::HttpServer::Server
 		
 		keys = []
 		vals = []
+		params = {}
 		has_parameters = false
 		if @http_query_string != nil and @http_request_method == "GET"
-			parse_request = @http_query_string.split('&')
-			if parse_request.length > 0 then
-				parse_request.each {|param|
-					kv_array = param.split('=')
-					keys.push kv_array[0]
-					vals.push CGI::unescape(kv_array[1])
-				}
-			end
+			has_parameters = true
+			params = CGI::parse(@http_query_string)
 		elsif @http_content != nil and @http_request_method == "POST"
-			parse_request = @http_content.split('&')
-			if parse_request.length > 0 then
-				parse_request.each {|param|
-					kv_array = param.split('=')
-					keys.push kv_array[0]
-					vals.push CGI::unescape(kv_array[1])
-				}
-			end			
+			has_parameters = true
+			params = CGI::parse(@http_content)
 		end
-		if keys.length >= 1
-			# Must be matching pairs of keys otherwise lets ignore it
-			# There must be a better way?
-			if keys.length == vals.length
-				has_parameters = true
-				puts "Keys: #{keys.inspect} Vals: #{vals.inspect}"
-			end
+		if params != {}
+			puts params.inspect
 		end
-
+		
 		# TODO: check @http_request_uri for the URL and then check other stuff
 		# @http_protocol for http or https
 		# @http_request_method for POST or GET
